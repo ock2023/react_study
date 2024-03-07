9a4a11e07f3893a26760e793204c20cb2f550eb1
ock2023 <dmsdhr2605@gmail.com>
2024-03-08 01:08:24 +0900

    side effect

--- section06/src/App.jsx
+++ section06/src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Controller from "./components/Controller";
 import Viewer from "./components/Viewer";
+import Even from "./components/Even";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+
+  // 1. 마운트 : 탄생
+  useEffect(() => {
+    console.log("mount!");
+  }, []);
+  // 2. 업데이트 : 변화, 리렌더링
+  useEffect(() => {
+    console.log("update!");
+  });
+  // 3. 언마운트 : 죽음
 
   const onClickBtn = (value) => {
     setCount(count + value);
@@ -14,7 +26,15 @@ function App() {
     <div className="App">
       <h1>Simple Counter</h1>
       <section>
+        <input
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        />
+      </section>
+      <section>
         <Viewer count={count} />
+        {count % 2 === 0 ? <Even /> : null}
       </section>
       <section>
         <Controller onClickBtn={onClickBtn} />
